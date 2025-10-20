fe9d3d5396da1b5ada121d34483fef763685bf78
Leo Nicolle <leo@linkurio.us>
2025-10-21 01:24:56 +0200

    setup ci

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  optimizeDeps: {
+    include: ['opencv-ts']
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index-vue.html')
+      }
+    }
+  }
+})
